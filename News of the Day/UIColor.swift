11aac1aff72b63a81ade4e2830e271681a671039
Evan Coleman <e@edc.me>
2016-12-01 14:11:02 -0500

    Remove old code

--- News of the Day/UIColor.swift
+++ News of the Day/UIColor.swift
@@ -17,15 +17,4 @@ extension UIColor {
         
         return brightness <= 125 ? .white : .black
     }
-    
-    class func colors(_ count: Int) -> [UIColor] {
-        var colors: [UIColor] = []
-        
-        let inc = 360.0 / Double(count)
-        for h in stride(from: 0, to: 360, by: inc) {
-            colors.append(UIColor(hue: CGFloat(h / 360.0), saturation: 1, brightness: 1, alpha: 1))
-        }
-        
-        return colors
-    }
 }
